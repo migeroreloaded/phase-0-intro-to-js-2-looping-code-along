f8bde70a585bcd57fa747edc7e115c012b7f0d8d
Michael Roche <mikeroche138@gmail.com>
2024-03-21 12:56:47 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(namesArray) {
+    let thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+}
+  
